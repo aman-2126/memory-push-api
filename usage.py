208ade9fb255cd7052723e3c8fa183287cf5dc6b
aman-2126 <99132548+aman-2126@users.noreply.github.com>
2022-02-20 14:54:05 +0530

    added functionality

--- usage.py
+++ usage.py
@@ -1,92 +1,69 @@
 from flask import Flask, request
 import gc
-app = Flask('__name__')
+import multiprocessing
 
+
+app = Flask('__name__')
 arr = list()
+memoryProcess = None
+
+   
+
+def use_memory(mode, arr):
+    
+    arr_len = 0
+    if mode == 'zero':
+        arr_len = 0
+    elif mode == 'low':
+        arr_len = 2
+    elif mode == 'med':
+        arr_len = 15
+    elif mode == 'high':
+        arr_len = 40
+
+    a = list()
+    i = 0
+    try:
+        while (len(a) < arr_len*1024*1024):
+            a.append(i)
+            i += 1
+    except Exception as error:
+        print("ran out of memory")
+    finally:
+        while(len(arr) > 0):
+            arr.pop(0)
+        gc.collect()
+        arr.append(a)
+
+    manipulating_arr = [False]
+    while(True):
+        a = 1
 
 
 @app.route('/')
 def index():
     return "Hello world"
+@app.route('/push-memory')
+def push_memory():
+    mode = request.args.get('mode', type = str, default = 'zero')
+    global memoryProcess
+    if(memoryProcess):
+        memoryProcess.terminate()
+        memoryProcess = None
+    memoryProcess = multiprocessing.Process(target = use_memory, args = (mode, arr))
+    memoryProcess.start()
+    
+    return f"pushing memory in mode {mode}"
+    
 
-
-@app.route('/low')
-def low():
-    a = list()
-    i = 0
-    try:
-        while(len(a) < 1024*1024*4):
-            i += 1
-            a.append(i)
-    except Exception as error:
-        print("ran out of memory")
-    finally:
-        while(len(arr) > 0):
-            arr.pop(0)
-        gc.collect()
-        arr.append(a)
-
-    return "low"
-
-
-@app.route('/med')
-def mid():
-    # arr = list()
-    a = list()
-    i = 0
-    try:
-        while(len(a) < 1024*1024*10):
-            i += 1
-            a.append(i)
-    except Exception as error:
-        print("ran out of memory")
-    finally:
-        while(len(arr) > 0):
-            arr.pop(0)
-        gc.collect()
-        arr.append(a)
-
-    return "mid"
-
-
-@app.route('/high')
-def high():
-    # arr = list()
-    a = list()
-    i = 0
-    try:
-        while(len(a) < 1024*1024*40):
-            i += 1
-            a.append(i)
-    except Exception as error:
-        print("ran out of memory")
-    finally:
-        while(len(arr) > 0):
-            arr.pop(0)
-        gc.collect()
-        arr.append(a)
-    return "high"
-
-
-@app.route('/no')
-def no():
-
-    print(len(arr))
-    try:
-        while(len(arr) > 0):
-            arr.pop(0)
-    except Exception as error:
-        print("empty array")
-    gc.collect()
-
-    return "no"
 
 
 if __name__ == '__main__':
-    # try:
-    #     while(len(arr) < 1024*1024*50):
-    #         arr.append(1)
-    # except Exception as error:
-    #     print("ran out of memory")
-
+    memoryProcess = None
     app.run()
+    if(memoryProcess):
+        memoryProcess.terminate()
+
+
+
+
